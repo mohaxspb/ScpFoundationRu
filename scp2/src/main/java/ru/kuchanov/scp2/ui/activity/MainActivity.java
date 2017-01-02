5808ce8256fdb522f3e6c5b1d95a96d1c42cbad2
mohax.spb <mohax.spb@gmail.com>
2017-01-02 23:52:54 +0300

    - just some refactoring

--- scp2/src/main/java/ru/kuchanov/scp2/ui/activity/MainActivity.java
+++ scp2/src/main/java/ru/kuchanov/scp2/ui/activity/MainActivity.java
@@ -37,5 +37,47 @@ public class MainActivity extends BaseDrawerActivity<Main.View, Main.Presenter> 
     public void onNavigationItemClicked(int id) {
         Timber.d("onNavigationItemClicked with id: %s", id);
         //TODO
+        String title = "";
+        switch (id) {
+            case R.id.about:
+                title = "Об организации";
+                break;
+            case R.id.news:
+                title = "Новости";
+                break;
+            case R.id.rate_articles:
+                title = "Статьи по рейтингу";
+                break;
+            case R.id.new_articles:
+                title = "Новые статьи";
+                break;
+            case R.id.objects_I:
+                title = "Объекты I";
+                break;
+            case R.id.objects_II:
+                title = "Объекты II";
+                break;
+            case R.id.objects_III:
+                title = "Объекты III";
+                break;
+            case R.id.objects_RU:
+                title = "Объекты RU";
+                break;
+            case R.id.favorite:
+                title = "Избранное";
+                break;
+            case R.id.offline:
+                title = "Офлайн";
+                break;
+            case R.id.stories:
+                title = "Рассказы";
+                break;
+            case R.id.files:
+                title = "Материалы";
+                break;
+            case R.id.site_search:
+                title = "Поиск";
+                break;
+        }
     }
 }
