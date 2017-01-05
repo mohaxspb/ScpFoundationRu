1d8cf1a7fc1c0c33a6564607c5736d5ea1b8e073
mohax.spb <mohax.spb@gmail.com>
2017-01-06 01:59:55 +0300

    - fix image dialog  - add photoView lib

--- scp2/src/main/java/ru/kuchanov/scp2/util/DialogUtils.java
+++ scp2/src/main/java/ru/kuchanov/scp2/util/DialogUtils.java
@@ -2,16 +2,12 @@ package ru.kuchanov.scp2.util;
 
 import android.app.Dialog;
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.Matrix;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.request.RequestListener;
-import com.bumptech.glide.request.target.Target;
 
-import it.sephiroth.android.library.imagezoom.ImageViewTouch;
 import ru.kuchanov.scp2.R;
 import timber.log.Timber;
+import uk.co.senab.photoview.PhotoView;
 
 /**
  * Created by mohax on 05.01.2017.
@@ -26,24 +22,13 @@ public class DialogUtils {
         nagDialog.setCancelable(true);
         nagDialog.setContentView(R.layout.preview_image);
 
-        final ImageViewTouch imageViewTouch = (ImageViewTouch) nagDialog.findViewById(R.id.image_view_touch);
+        final PhotoView photoView = (PhotoView) nagDialog.findViewById(R.id.image_view_touch);
+        photoView.setMaximumScale(5f);
 
         Glide.with(ctx)
                 .load(imgUrl)
-                .asBitmap()
-                .listener(new RequestListener<String, Bitmap>() {
-                    @Override
-                    public boolean onException(Exception e, String model, Target<Bitmap> target, boolean isFirstResource) {
-                        return false;
-                    }
-
-                    @Override
-                    public boolean onResourceReady(Bitmap resource, String model, Target<Bitmap> target, boolean isFromMemoryCache, boolean isFirstResource) {
-                        Matrix matrix = imageViewTouch.getDisplayMatrix();
-                        imageViewTouch.setImageBitmap(resource, matrix, 0.5f, 2.0f);
-                        return true;
-                    }
-                });
+                .placeholder(R.drawable.ic_image_white_48dp)
+                .into(photoView);
 
         nagDialog.setOnCancelListener(dialog -> {
             System.out.println("nagDialog.onCancel ArtActivity");
