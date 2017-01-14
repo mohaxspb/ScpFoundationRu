4bf546d12679ba755bb6ab1657b45df2c0414950
mohax.spb <mohax.spb@gmail.com>
2017-01-14 03:50:20 +0300

    - add notification Manager for newArtsNotification  - add setting alarmManager after but and checj for it while mainActivityStart  - add prefs methods for notifications (enable, period, LED, vibrate, sound)  - add ic_launcher  - increase build

--- app/src/main/java/ru/dante/scpfoundation/utils/NotificationUtils.java
+++ app/src/main/java/ru/dante/scpfoundation/utils/NotificationUtils.java
@@ -15,18 +15,13 @@ import ru.dante.scpfoundation.receivers.ReceiverTimer;
 /**
  * Created for MyApplication by Dante on 20.03.2016  18:13.
  */
-public class NotificationUtils
-{
-    private static final int ID=999;
+public class NotificationUtils {
+    private static final int ID = 999;
     private static final String LOG = NotificationUtils.class.getSimpleName();
     public static final String RECEIVER_TIMER_ACTION = "ru.dante.scpfoundation.receivers.ReceiverTimer";
 
-    public static void setAlarm(Context ctx)
-    {
+    public static void setAlarm(Context ctx) {
         Log.i(LOG, "Setting alarm");
-//        if (Build.VERSION.SDK_INT<Build.VERSION_CODES.KITKAT){
-//            return;
-//        }
         PreferenceManager.setDefaultValues(ctx, R.xml.pref_design, true);
         PreferenceManager.setDefaultValues(ctx, R.xml.pref_notification, true);
         PreferenceManager.setDefaultValues(ctx, R.xml.pref_system, true);
@@ -46,17 +41,14 @@ public class NotificationUtils
         long periodInMiliseconds = periodInMinutes * 60 * 1000;
 //        //TODO test
 //        periodInMiliseconds = 1000 * 20;
-        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT)
-        {
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             am.setInexactRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + periodInMiliseconds, periodInMiliseconds, pendingIntent);
-        } else
-        {
+        } else {
             am.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + periodInMiliseconds, periodInMiliseconds, pendingIntent);
         }
     }
 
-    public static void checkAlarm(Context ctx)
-    {
+    public static void checkAlarm(Context ctx) {
         Log.i(LOG, "checkAlarm");
 //        if (Build.VERSION.SDK_INT<Build.VERSION_CODES.KITKAT){
 //            return;
@@ -71,44 +63,37 @@ public class NotificationUtils
                 PendingIntent.FLAG_NO_CREATE) != null);
         SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(ctx);
 
-        if (alarmUp)
-        {
+        if (alarmUp) {
             //Log.d("myTag", "Alarm is already active");
             Log.i(LOG, "Alarm is already active");
             boolean isNotificationOn = pref.getBoolean(ctx.getString(R.string.pref_notifications_key_enable), false);
-            if (!isNotificationOn)
-            {
+            if (!isNotificationOn) {
                 Log.i(LOG, "But must not be, so...");
                 NotificationUtils.cancelAlarm(ctx);
-            } else
-            {
+            } else {
                 Log.i(LOG, "So do nothing");
             }
-        } else
-        {
+        } else {
             Log.i(LOG, "Alarm IS NOT active");
 
             boolean isNotificationOn = pref.getBoolean(ctx.getString(R.string.pref_notifications_key_enable), false);
-            if (isNotificationOn)
-            {
+            if (isNotificationOn) {
                 Log.i(LOG, "But must be, so...");
                 NotificationUtils.setAlarm(ctx);
-            } else
-            {
+            } else {
                 Log.i(LOG, "So do nothing");
             }
         }
     }
 
-    public static void cancelAlarm(Context ctx)
-    {
+    public static void cancelAlarm(Context ctx) {
         Log.i(LOG, "Canceling alarm");
 //        if (Build.VERSION.SDK_INT<Build.VERSION_CODES.KITKAT){
 //            return;
 //        }
         final AlarmManager am = (AlarmManager) ctx.getApplicationContext().getSystemService(Context.ALARM_SERVICE);
         Intent intentToTimerReceiver = new Intent(ctx.getApplicationContext(), ReceiverTimer.class);
-       intentToTimerReceiver.setAction(RECEIVER_TIMER_ACTION);
+        intentToTimerReceiver.setAction(RECEIVER_TIMER_ACTION);
         PendingIntent pendingIntent = PendingIntent.getBroadcast(ctx.getApplicationContext(), ID,
                 intentToTimerReceiver,
                 PendingIntent.FLAG_CANCEL_CURRENT);
---
+++ scp2/src/main/java/ru/kuchanov/scp2/receivers/ReceiverTimer.java
@@ -0,0 +1,168 @@
+package ru.kuchanov.scp2.receivers;
+
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import ru.kuchanov.scp2.Constants;
+import ru.kuchanov.scp2.MyApplication;
+import ru.kuchanov.scp2.R;
+import ru.kuchanov.scp2.api.ApiClient;
+import ru.kuchanov.scp2.db.DbProvider;
+import ru.kuchanov.scp2.db.DbProviderFactory;
+import ru.kuchanov.scp2.db.model.Article;
+import ru.kuchanov.scp2.manager.MyPreferenceManager;
+import ru.kuchanov.scp2.ui.activity.MainActivity;
+import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
+import timber.log.Timber;
+
+import static ru.kuchanov.scp2.Constants.Api.NUM_OF_ARTICLES_ON_RECENT_PAGE;
+
+public class ReceiverTimer extends BroadcastReceiver {
+
+    public static final int NOTIF_ID = 963;
+
+    @Inject
+    MyPreferenceManager mMyPreferencesManager;
+    @Inject
+    DbProviderFactory mDbProviderFactory;
+    @Inject
+    ApiClient mApiClient;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Timber.d("onReceive with action: %s", intent.getAction());
+        if (context.getString(R.string.receiver_action_timer).equals(intent.getAction())) {
+            MyApplication.getAppComponent().inject(this);
+            download(context);
+        }
+    }
+
+    protected void download(Context context) {
+        mApiClient.getRecentArticlesForPage(1)
+                .flatMap(articles -> {
+                    DbProvider dbProvider = mDbProviderFactory.getDbProvider();
+                    List<Article> newArticles = new ArrayList<>();
+                    for (Article apiArticle : articles) {
+                        Article inDbArticle = dbProvider.getArticleSync(apiArticle.url);
+                        if (inDbArticle == null) {
+                            //so its new one, increase counter
+                            newArticles.add(apiArticle);
+                        }
+                    }
+                    dbProvider.close();
+                    return Observable.just(newArticles);
+                })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .flatMap(newArticles -> mDbProviderFactory.getDbProvider()
+                        .saveRecentArticlesList(newArticles, Constants.Api.ZERO_OFFSET)
+                        .flatMap(savedArts -> Observable.just(newArticles)))
+                .subscribe(
+                        result -> {
+                            sendNotification(context, result);
+                        }
+                        , error -> Timber.e(error, "error while getRecentArts"));
+    }
+
+    public void sendNotification(Context ctx, List<Article> dataFromWeb) {
+        // Use NotificationCompat.Builder to set up our notification.
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(ctx);
+
+        //icon appears in device notification bar and right hand corner of notification
+        builder.setSmallIcon(R.drawable.ic_launcher);
+
+        //Set the text that is displayed in the status bar when the notification first arrives.
+        builder.setTicker(dataFromWeb.get(0).title);
+
+        // This intent is fired when notification is clicked
+        Intent intent = new Intent(ctx, MainActivity.class);
+        intent.putExtra(MainActivity.EXTRA_LINK, Constants.Urls.NEW_ARTICLES);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(ctx, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+        // Set the intent that will fire when the user taps the notification.
+        builder.setContentIntent(pendingIntent);
+
+        // Large icon appears on the left of the notification
+        builder.setLargeIcon(BitmapFactory.decodeResource(ctx.getResources(), R.drawable.ic_launcher));
+
+        // Content title, which appears in large type at the top of the notification
+        //		builder.setContentTitle("Новые статьи");
+
+        // Content text, which appears in smaller text below the title
+        //				builder.setContentText("Новые статьи");
+
+        // The subtext, which appears under the text on newer devices.
+        // This will show-up in the devices with Android 4.2 and above only
+        builder.setSubText(dataFromWeb.get(0).title);//"Всего новых статей:");
+
+        builder.setAutoCancel(true);
+
+        NotificationCompat.InboxStyle inboxStyle = new NotificationCompat.InboxStyle();
+        if (dataFromWeb.size() == NUM_OF_ARTICLES_ON_RECENT_PAGE) {
+            String[] events = new String[dataFromWeb.size()];
+            //TODO save to res
+            inboxStyle.setBigContentTitle("Новые статьи: " + dataFromWeb.size() + "+");
+            // Moves events into the expanded layout
+            for (int i = 0; i < events.length; i++) {
+                events[i] = dataFromWeb.get(i).title;
+                inboxStyle.addLine(events[i]);
+            }
+            builder.setNumber(dataFromWeb.size());
+        } else {
+            //to test
+            //newQuont = "10";
+            String[] events = new String[dataFromWeb.size()];
+            // Sets a title for the Inbox in expanded layout
+            inboxStyle.setBigContentTitle("Новые статьи: " + dataFromWeb.size());
+            // Moves events into the expanded layout
+            for (int i = 0; i < events.length; i++) {
+                events[i] = dataFromWeb.get(i).title;
+                inboxStyle.addLine(events[i]);
+            }
+            builder.setNumber(dataFromWeb.size());
+        }
+
+        // Moves the expanded layout object into the notification object.
+        builder.setStyle(inboxStyle);
+
+        // Content title, which appears in large type at the top of the notification
+        builder.setContentTitle("Новые статьи: " + dataFromWeb.size());
+        if (mMyPreferencesManager.isNotificationVibrationEnabled()) {
+            //TODO move to const
+            builder.setVibrate(new long[]{500, 500, 500, 500, 500});
+        }
+        //LED
+        if (mMyPreferencesManager.isNotificationLedEnabled()) {
+            //TODO move to const
+            builder.setLights(Color.WHITE, 3000, 3000);
+        }
+        //Sound//LED
+        if (mMyPreferencesManager.isNotificationSoundEnabled()) {
+            Uri alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+            builder.setSound(alarmSound);
+        }
+
+        NotificationManager notificationManager = (NotificationManager) ctx.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // Will display the notification in the notification bar
+        //TODO move to const
+        notificationManager.notify(NOTIF_ID, builder.build());
+    }
+}
