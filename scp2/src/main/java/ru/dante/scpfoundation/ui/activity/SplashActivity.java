88be4c00987e2f44f228c7674b656d2335c2c31d
Ivan Semkin <ivan@semkin.ru>
2017-04-21 01:22:37 +0300

    splash screen

---
+++ scp2/src/main/java/ru/dante/scpfoundation/ui/activity/SplashActivity.java
@@ -0,0 +1,17 @@
+package ru.dante.scpfoundation.ui.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        startActivity(new Intent(this, LicenceActivity.class));
+        finishAffinity();
+    }
+
+}
